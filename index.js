2cca58394a31890951864c2bdbdc0e847f560b4b
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-01 22:23:44 -0600

    commit tree 8ca93543c0a78d38fd988ab49b7621570dbd949d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+//create variable driver assigned to Object
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    //do not mutate, return new driver w/ updated value for key passed in
+    return Object.assign({}, driver, { [key]: value } );
+    // returns driver { key: value }
+    // creates a copy w/in funct. 
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    // like update above, but mutate.
+    driver[key] = value;
+    return driver;
+    //adds to actual variable we created at the top
+};
+
+function deleteFromDriverByKey(driver, key) {
+    // delete k-v pair for the key passed in from obj. Do not mutate.
+    const newObject = Object.assign({}, driver);
+    // creates copy then deletes key from our copy
+
+    delete newObject[key];
+    return newObject
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    // work the same as above, but mutate
+    //deletes key from vari. created at top
+    delete driver[key];
+    return driver
+};
